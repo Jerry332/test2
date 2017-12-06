0b920c534cd6b172fd1ac5402916fb16653bf36c
Jerry332 <zeeef@163.com>
2017-12-07 00:58:58 +0800

    Signed-off-by: Jerry332 <zeeef@163.com>

---
+++ MethodOverload/src/MethodOverload.java
@@ -0,0 +1,20 @@
+class overload{
+	int add(int a,int b)
+	{return a+b;}
+	int add (int a,int b,int c){
+		return a+b+c;
+	}
+}
+
+public class MethodOverload {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		overload test=new overload();
+     System.out.println(test.add(1, 2));
+     System.out.println(test.add(1, 2, 3));
+	
+	}
+}
