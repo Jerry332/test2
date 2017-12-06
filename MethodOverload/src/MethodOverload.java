ef1cd88bdeccdd718b8a5a123b40d000dd02478f
Jerry332 <zeeef@163.com>
2017-12-07 01:01:11 +0800

    Update MethodOverload.java

--- MethodOverload/src/MethodOverload.java
+++ MethodOverload/src/MethodOverload.java
@@ -13,7 +13,7 @@ public class MethodOverload {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		overload test=new overload();
-     System.out.println(test.add(1, 2));
+     System.out.println(test.add(1, 3));
      System.out.println(test.add(1, 2, 3));
 	
 	}
